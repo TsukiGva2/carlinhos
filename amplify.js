d86be44b666854a1023f018f6dbbe21aeb502a58
tortea <tsukigva@gmail.com>
2023-03-25 02:44:47 -0300

    kkkkkkkkkkkkkkkkkk

---
+++ amplify.js
@@ -0,0 +1,16 @@
+function amp(e, n) {
+	let context = new (window.AudioContext || window.webkitAudioContext);
+	let result = {
+		context: context,
+		source: context.createMediaElementSource(e),
+		gain: context.createGain(),
+		media: e,
+		amplify: n => result.gain.gain.value = n,
+		getAmpLevel: () => result.gain.gain.value
+	};
+	result.source.connect(result.gain);
+	result.gain.connect(context.destination);
+	result.amplify(n);
+	return result;
+}
+
