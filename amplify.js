dda35c9c9409fdcae347bf27d78007f8056eea1f
tortea <tsukigva@gmail.com>
2023-03-25 03:52:48 -0300

    tentativa e erro

--- amplify.js
+++ amplify.js
@@ -1,7 +1,5 @@
 function amp(e, n) {
 	let context = new (window.AudioContext || window.webkitAudioContext);
-	let debugp = document.getElementById("debugp");
-	debugp.innerHTML = context;
 	let result = {
 		context: context,
 		source: context.createMediaElementSource(e),
@@ -10,11 +8,10 @@ function amp(e, n) {
 		amplify: n => result.gain.gain.value = n,
 		getAmpLevel: () => result.gain.gain.value
 	};
-	debugp.innerHTML += result;
+
 	result.source.connect(result.gain);
 	result.gain.connect(context.destination);
 	result.amplify(n);
-	debugp.innerHTML += result;
 	return result;
 }
 
